ba5e2af85f3f6bc8d540d3181aff3e17e80f1bba
Roman Gritsik <111363152+Binbinik@users.noreply.github.com>
2023-05-30 15:03:52 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </li>
+        `;
+    })
+    .join("");
+}
+
+const galleryMarkup = createGalleryItems(galleryItems);
+const galleryList = document.querySelector(".gallery");
+galleryList.innerHTML = galleryMarkup;
 console.log(galleryItems);
